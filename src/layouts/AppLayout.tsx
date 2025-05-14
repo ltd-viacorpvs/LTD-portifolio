1b92f621589169493afeac80ad02c737caed30c0
Mateus Bessa <mateusbessacontato@gmail.com>
2025-05-14 13:34:44 -0300

    feat: header almost implemented

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -4,6 +4,23 @@ import { Outlet } from 'react-router-dom'
 export const AppLayout = () => {
 	return (
 		<div className="flex min-h-screen flex-col antialiased">
+				<header className="bg-white p-4 border-b border-gray-200 shadow-md">
+					<div className="container mx-auto flex justify-between items-center">
+						<div className="flex items-center space-x-4">
+							<p>LOGO</p>
+						</div>
+						
+						<nav className="hidden md:flex space-x-6">
+							<a href="#" className="text-gray-800 hover:text-blue-600">Home</a>
+							<a href="#" className="text-gray-800 hover:text-blue-600">Sobre</a>
+							<a href="#" className="text-gray-800 hover:text-blue-600">Projetos</a>
+							<a href="#" className="text-gray-800 hover:text-blue-600">Blog</a>
+							<a href="#" className="text-blue-600 font-medium flex items-center">
+								Contact-nos <span className="ml-1">→</span>
+							</a>
+						</nav>
+					</div>
+      </header>
 			<Outlet />
 			<Footer />
 		</div>
